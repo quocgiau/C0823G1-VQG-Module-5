a7a7bb03a8a2c5e8edf43b8a69abc45c23d88fe6
Quốc Giàu <quocgiau61s@gmail.com>
2024-02-28 16:11:11 +0700

    Giau up

--- ss1_es6/bai2.js
+++ ss1_es6/bai2.js
@@ -14,3 +14,22 @@ let isPrime = (number) => {
 };
 console.log(isPrime(7));
 console.log(isPrime(10));
+let prime = (number) => {
+    if (number <= 1) {
+        return false;
+    }
+
+    for (let i = 2; i <= Math.sqrt(number); i++) {
+        if (number % i === 0) {
+            return false;
+        }
+    }
+
+    return true;
+};
+
+let numbers = [2, 3, 4, 5, 6, 7, 8, 9, 10];
+let primeNumbers = numbers.filter(prime);
+
+console.log(primeNumbers);
+// Bai 2:
---
+++ ss2/student-list/src/App.js
@@ -0,0 +1,60 @@
+import logo from './logo.svg';
+import './App.css';
+import './index.css';
+
+function App() {
+  let students = [
+    {
+      company: 'Alfreds Futterkiste',
+      contact: 'Maria Anders',
+      country: 'Germany'
+    },
+    {
+      company: 'Centro comercial Moctezuma',
+      contact: 'Francisco Chang',
+      country: 'Mexico'
+    },
+    {
+      company: 'Ernst Handel',
+      contact: 'Roland Mendel',
+      country: 'Austria'
+    },
+    {
+      company: 'Island Trading',
+      contact: 'Helen Bennett',
+      country: 'UK'
+    },
+    {
+      company: 'Laughing Bacchus Winecellars',
+      contact: 'Yoshi Tannamuri',
+      country: 'Canada'
+    },
+    {
+      company: 'Magazzini Alimentari Riuniti',
+      contact: 'Giovanni Rovelli',
+      country: 'Italy'
+    }
+  ]
+  return (
+    <div className="App">
+      <table>
+        <tbody>
+        <tr>
+          <th>Company</th>
+          <th>Contact</th>
+          <th>Country</th>
+        </tr>
+        </tbody>
+        {students.map(student => (
+            <tr>
+              <td>{student.company}</td>
+              <td>{student.contact}</td>
+              <td>{student.country}</td>
+            </tr>
+        ))}
+      </table>
+    </div>
+  );
+}
+
+export default App;
