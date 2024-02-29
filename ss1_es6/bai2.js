298d18ba3adccc29b28eb4ea4ac94c8e2aa039aa
Quốc Giàu <quocgiau61s@gmail.com>
2024-02-29 09:06:26 +0700

    Cap nhat

--- ss1_es6/bai2.js
+++ ss1_es6/bai2.js
@@ -32,4 +32,45 @@ let numbers = [2, 3, 4, 5, 6, 7, 8, 9, 10];
 let primeNumbers = numbers.filter(prime);
 
 console.log(primeNumbers);
+
 // Bai 2:
+const person = {
+    firstName: 'John',
+    lastName: 'Doe',
+    age: 30,
+    gender: 'male',
+    occupation: 'developer',
+    nationality: 'American',
+    city: 'New York',
+    hobbies: ['reading', 'traveling', 'photography'],
+    languages: ['English', 'Spanish'],
+    education: {
+        degree: 'Bachelor',
+        major: 'Computer Science',
+        university: 'Harvard University'
+    }
+};
+let {firstName, gender, education: {degree}, languages} = person;
+let englishOnly = languages.filter(language => language === 'English');
+
+let student = {firstName, gender, degree, languages: englishOnly};
+
+console.log(student);
+
+// Bài 3:
+function displayInfo({firstName = "Quân", degree = "NA"}) {
+    console.log(`First Name: ${firstName}`);
+    console.log(`Degree: ${degree}`);
+}
+
+let person1 = {
+    firstName: "John",
+    degree: "Bachelor",
+};
+
+let person2 = {
+    degree: "Master",
+};
+
+displayInfo(person1);
+displayInfo(person2);
